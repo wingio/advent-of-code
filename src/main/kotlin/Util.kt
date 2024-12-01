49f417be58c11c7aa8c089f9ef566dc3f253d856
Wing <44992537+wingio@users.noreply.github.com>
2024-12-01 00:41:02 -0500

    Solve Day 1

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,10 +1,16 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
+private val whitespaceRx = "\\s+".toRegex()
+
 fun <T> T?.print(transform: (T) -> Any? = { this }) = println(this?.run(transform))
 
 fun readInput(day: Int): String {
     return Path("./src/main/resources/Day$day.txt")
         .readText()
         .replace("\r\n", "\n") // We love Windows
+}
+
+fun getNumbersFromLine(line: String): List<Int> {
+    return line.split(whitespaceRx).map { it.toInt() }
 }
--- src/main/kotlin/puzzles/Day1.kt
+++ src/main/kotlin/puzzles/Day1.kt
@@ -1,11 +1,30 @@
 package puzzles
 
+import getNumbersFromLine
+import print
 import readInput
+import kotlin.math.absoluteValue
 
 // Day 1
 
 private val input = readInput(day = 1)
 
 fun main() {
+    val numbers = input.lines().map { getNumbersFromLine(it) }
+    val list1 = numbers.map { it.first() }.sorted()
+    val list2 = numbers.map { it.last() }.sorted()
 
+    // Part 1
+    list1
+        .zip(list2)
+        .sumOf { (it.first - it.second).absoluteValue }
+        .print { "Solution to part 1: $it" }
+
+    // Part 2
+    list1
+        .sumOf { item1 ->
+            item1 * list2.count { item2 -> item2 == item1 }
+        }
+        .print { "Solution to part 2: $it" }
 }
+

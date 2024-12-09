899a5394804234c2ab52bd6f8419cd63f764bd89
Wing <44992537+wingio@users.noreply.github.com>
2024-12-08 20:00:16 -0500

    Update `day` dsl to improve organization

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,19 +1,16 @@
 import kotlin.time.measureTimedValue
 
 fun day(day: Int, builder: Day.() -> Unit) {
-    Day(day).apply(builder)()
+    Day(day).apply(builder)
 }
 
 class Day(
-    private val day: Int
+    day: Int
 ) {
 
     val input = readInput(day)
 
-    private var partOne: () -> Any = {}
-    private var partTwo: () -> Any = {}
-
-    operator fun invoke() {
+    init {
         println("""
             -+-=========================+-+{[ * ]}+-+=========================-+-
                                      Advent Of Code 2024
@@ -21,30 +18,27 @@ class Day(
             -+-------===================================================-------+-
         """.trimIndent())
         println("\n")
-
-        val (result1,  time1) = measureTimedValue(partOne)
-        println("""
-            -+{[ Part 1 ]}=====================================================+-
-            Result: $result1
-            Took $time1
-        """.trimIndent())
-
-        println("\n")
-
-        val (result2,  time2) = measureTimedValue(partTwo)
-        println("""
-            -+{[ Part 2 ]}=====================================================+-
-            Result: $result2
-            Took $time2
-        """.trimIndent())
     }
 
     fun part1(block: () -> Any) {
-        partOne = block
+        measureTimedValue(block).print { (result, time) ->
+            """
+                -+{[ Part 1 ]}=====================================================+-
+                Result: $result
+                Took $time
+            """.trimIndent()
+        }
+        println()
     }
 
     fun part2(block: () -> Any) {
-        partTwo = block
+        measureTimedValue(block).print { (result, time) ->
+            """
+                -+{[ Part 2 ]}=====================================================+-
+                Result: $result
+                Took $time
+            """.trimIndent()
+        }
     }
 
 }

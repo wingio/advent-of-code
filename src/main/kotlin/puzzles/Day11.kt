e051b14019c7228684091a8be344e095bbf33067
Wing <44992537+wingio@users.noreply.github.com>
2024-12-11 02:24:47 -0500

    Solve Day 11

---
+++ src/main/kotlin/puzzles/Day11.kt
@@ -0,0 +1,39 @@
+package puzzles
+
+import day
+
+// Day 11
+
+fun main() = day(11) {
+    val stones = input.split(" ").map { it.toLong() }
+
+    part1 {
+        val stoneMap = mutableMapOf<Pair<Int, Long>, Long>()
+        stones.sumOf { change(it, 0, 25, stoneMap) }
+    }
+
+    part2 {
+        val stoneMap = mutableMapOf<Pair<Int, Long>, Long>()
+        stones.sumOf { change(it, 0, 75, stoneMap) }
+    }
+}
+
+fun change(stone: Long, depth: Int, maxDepth: Int, cache: MutableMap<Pair<Int, Long>, Long>): Long {
+    if(depth == maxDepth) return 1
+    cache[depth to stone]?.let { return it }
+
+    cache[depth to stone] = when {
+        stone == 0L -> change(1, depth + 1, maxDepth, cache)
+        stone.toString().length % 2 == 0 -> {
+            val str = stone.toString()
+            val firstHalf = str.substring(0..<str.length / 2).toLong()
+            val secondHalf = str.substring(str.length / 2).toLong()
+            change(firstHalf, depth + 1, maxDepth, cache) + change(secondHalf, depth + 1, maxDepth, cache)
+        }
+        else -> {
+            change(stone * 2024, depth + 1, maxDepth, cache)
+        }
+    }
+
+    return cache[depth to stone]!!
+}

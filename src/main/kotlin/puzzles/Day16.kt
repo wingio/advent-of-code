2d13ec8c7befd561d5524bbc46e0f13198c0061f
Wing <44992537+wingio@users.noreply.github.com>
2024-12-16 01:39:07 -0500

    Solve Day 16

---
+++ src/main/kotlin/puzzles/Day16.kt
@@ -0,0 +1,60 @@
+package puzzles
+
+import day
+import util.grid.*
+import java.util.PriorityQueue
+
+// Day 16
+
+fun main() = day(16, example = true) {
+    val maze = Grid.charGrid(input)
+
+    part1 { maze.dijkstra().first }
+    part2 { maze.dijkstra().second.size }
+}
+
+fun Grid<Char>.dijkstra(): Pair<Int, Set<Point>> {
+    val queue = PriorityQueue<Path>(compareBy { it.score })
+    val start = pointOfFirst { it == 'S' }
+    val end = pointOfFirst { it == 'E' }
+
+    queue.add(Path(0, listOf(start), Directions.East))
+
+    var score = Int.MAX_VALUE
+    val scores = mutableMapOf<Pair<Point, Direction>, Int>()
+    val seats = mutableSetOf<Point>()
+
+    while (queue.isNotEmpty()) {
+        val node = queue.poll()
+        val key = node.end to node.direction
+
+        if (node.end == end) { // We did it :)
+            if (node.score <= score) score = node.score else break
+            seats.addAll(node.points)
+        }
+
+        if (scores.containsKey(key) && scores[key]!! < node.score) continue // Don't revisit points with a worse score, should keep us out of loops
+        scores[key] = node.score
+
+        if (this[node.end + node.direction] != '#') queue.add(node.move()) // As long as there isn't a wall, we can proceed forwards
+        queue.add(node.turn(cw = false))
+        queue.add(node.turn(cw = true))
+    }
+
+    return score to seats
+}
+
+fun Direction.turn(cw: Boolean): Direction
+    = Directions.CARDINALS[(Directions.CARDINALS.indexOf(this) + if (cw) 1 else -1).mod(4)]
+
+data class Path(
+    val score: Int,
+    val points: List<Point>,
+    val direction: Direction
+) {
+
+    val end get() = points.last()
+    fun turn(cw: Boolean) = copy(score = score + 1000, direction = direction.turn(cw))
+    fun move() = copy(score = score + 1, points = points + (end + direction))
+
+}

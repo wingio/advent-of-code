e0f9dcfa37f85f51ba8fa28748c91cb742b7c50d
Wing <44992537+wingio@users.noreply.github.com>
2025-12-01 15:05:49 -0500

    2025 Day 1

--- aoc/solver/src/commonMain/kotlin/solver/solutions/Day01.kt
+++ aoc/solver/src/commonMain/kotlin/solver/solutions/Day01.kt
@@ -1,13 +1,34 @@
 package solver.solutions
 
 import solver.day
+import kotlin.math.abs
+
+private const val DialNums = 100
 
 val Day01 = day(1) { input ->
-    part1 {
+    val turns = input.lines().map {
+        if (it.startsWith('R')) it.drop(1).toInt() else -it.drop(1).toInt()
+    }
 
+    part1 {
+        var current = 50
+        turns.count { turn ->
+            current += turn
+            current % DialNums == 0
+        }
     }
 
     part2 {
+        var current = 50
+        var count = 0
 
+        turns.forEach { turn ->
+            val prev = current
+            count += abs(turn) / DialNums
+            current += turn % DialNums
+            if (prev != 0 && current !in 1..<DialNums) count++
+            current = (current + DialNums) % DialNums
+        }
+        count
     }
 }
